5dbc9e39ebe94eaa1281c97129e354fea4f120e1
httt <httt_@outlook.com>
2024-06-13 17:13:54 +0800

    店铺营业状态设置业务代码开发完成 调整了目录结构以适应后续用户端开发

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,29 @@
+package com.sky.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+// Spring Data Redis中提供了一个高度封装的类：RedisTemplate
+// 对相关api进行了归类封装,将同一类型操作封装为operation接口
+@Configuration
+@Slf4j
+// 编写配置类，创建RedisTemplate对象
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        log.info("开始创建redis模板对象...");
+        RedisTemplate redisTemplate = new RedisTemplate();
+        //设置redis的连接工厂对象
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //设置redis key的序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+}
