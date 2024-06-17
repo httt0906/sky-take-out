9965fb2bfe5e01ceb24b225b7f222b4b621def86
httt <httt_@outlook.com>
2024-06-17 12:56:27 +0800

    用户支付业务代码开发完成

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -10,5 +10,9 @@ public interface UserMapper {
     @Select("select * from user where openid = #{openid}")
     User getByOpenid(String openid);
 
+    @Select("select * from user where id=#{id}")
+    User getById(Long userId);
+
+
     void insert(User user);
 }
